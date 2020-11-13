7f67af511b34523f474101c36f29f921d7cab696
Benjamin Tzschoppe <Benjamin.Tzschoppe@oneteam.global>
2020-11-13 16:05:11 +0800

    first commit

---
+++ app_name/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the app_name index.")
+
+
+def landing(request):
+    context = {'example_var': 'Hello World!'}
+    return render(request, 'landing.html', context)
+
+
+def dashboard(request):
+    context = {'example_var': 'Hello World!'}
+    return render(request, 'dashboard.html', context)
